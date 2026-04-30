6f4b02f0b4ba297fd72c34d7c7545e858da5d06c
NelsonEUrrutia <85139793+NelsonEUrrutia@users.noreply.github.com>
2026-04-30 15:58:39 -0600

    Try to print files by directories

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import subprocess
 import sys
 
-branch = "euphoria-collab-launch"
-base = "stage"
+branch = "migration-bash-to-python"
+base = "main"
 added_files = []
 modified_files = []
 deleted_files = []
@@ -36,21 +36,52 @@ diff_output_files = diff_output_process.stdout.replace("\t", "::").split("\n")
 
 for file in diff_output_files:
     if "A" in file:
-        added_files.append(file)
+        file_dir = file.replace("A::", "")
+        added_files.append(file_dir)
 
     if "M" in file:
+        file_dir = file.replace("M::", "")
         modified_files.append(file)
 
     if "D" in file:
+        file_dir = file.replace("D::", "")
         deleted_files.append(file)
 
+
+def get_parent_dirs(files):
+    parent_dirs = []
+    for item in files:
+        split_path = item.split("/")
+        if len(split_path) == 1:
+            if "root" not in parent_dirs:
+                parent_dirs.append("root")
+        else:
+            parent_path = split_path[0]
+            if parent_path not in parent_dirs:
+                parent_dirs.append(parent_path)
+    return parent_dirs
+
+
+def print_files(parent_dirs, files):
+    for parent_dir in parent_dirs:
+        print(f"{parent_dir}")
+        for file in files:
+            split_path = file.split("/")
+            if len(split_path) == 1:
+                print(f"\n {file}")
+            else:
+                inner_files = file.replace(f"{parent_dir}/", "")
+                print(f"\n {inner_files}")
+        print("\n")
+
+
+added_parent_dirs = get_parent_dirs(added_files)
+modified_files_dirs = get_parent_dirs(modified_files)
+deleted_files_dirs = get_parent_dirs(deleted_files)
+
 print("ADDED")
-print(added_files)
-print("--\n")
-
+print_files(added_parent_dirs, added_files)
 print("MODIFIED")
-print(modified_files)
-print("--\n")
-
-print("DELETED")
-print(deleted_files)
+print_files(modified_files_dirs, modified_files)
+print("DELTED")
+print_files(deleted_files_dirs, deleted_files)
